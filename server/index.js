dd722e8fc5b5c28a183e6abf6eb7e778f89de55a
yjj <jmyoon8@naver.com>
2021-11-08 10:27:27 +0900

    호스팅준비

--- server/index.js
+++ server/index.js
@@ -2,13 +2,10 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const port = process.env.PORT || 2000;
-const router = require("./router/customers");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 //이미지 파일이 불러지는 위치를 체인지
-app.use("/images", express.static(__dirname + "/images"));
-app.use("/api/customers", router);
 
 app.get("/api", (req, res) => {
   res.send("hello mysql and node.js Express");
